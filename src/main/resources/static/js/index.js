491b655984a749478524b3d6dd61a55951bb71ed
LazarDukov <lazarrumenowdukow@gmail.com>
2024-12-31 17:49:20 +0200

    js configured for login button and new things learned

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,7 @@
-let logInButton = document.createElement('log-in');
-logInButton.textContent = 'Login';
-logInButton.addEventListener("mouseover", () => logInButton.style.color = 'gray');
-logInButton.addEventListener("click", () => window.location.href = '/user/login');
-document.getElementById('logInButton').appendChild(logInButton);
-
-let registerButton = document.createElement('register');
-registerButton.textContent = 'Register';
-registerButton.addEventListener("click", () => window.location.href = '/api/register');
-document.getElementById('registerButton').appendChild(registerButton);
+document.addEventListener("DOMContentLoaded", function () {
+    const loginButton = document.getElementById("loadLoginPage");
+    loginButton.addEventListener("click", loadLoginPage);
+});
+function loadLoginPage() {
+    window.location.href = "http://localhost:8080/user/login";
+}
