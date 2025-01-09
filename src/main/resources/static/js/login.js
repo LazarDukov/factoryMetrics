76d98d6c22c6e255a461d88410cd3d20be172051
LazarDukov <lazarrumenowdukow@gmail.com>
2025-01-09 23:45:39 +0200

    login validation made by js

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,24 +1,68 @@
 const submitButton = document.getElementById("submitButton");
 const workerIdentity = document.querySelector('#workerIdentity');
+const password = document.querySelector('#password');
 
-function showValidationMessage(input, message) {
-    let messageElement = input.nextElementSibling;
-    messageElement = document.createElement('span');
-    messageElement.classList.add('validation-message');
-    messageElement.style.color = 'red';
-    input.parentNode.insertBefore(messageElement, input.nextElementSibling);
+function showValidationMessageWorkerIdentity(workerIdentityInput, message) {
+    let messageElement = workerIdentityInput.nextElementSibling;
+    if (!messageElement || !messageElement.classList.contains('validation-message')) {
+        messageElement = document.createElement('span');
+        messageElement.classList.add('validation-message');
+        messageElement.style.color = 'red';
+        workerIdentityInput.parentNode.insertBefore(messageElement, workerIdentityInput.nextElementSibling);
+    }
+
     messageElement.textContent = message;
 }
 
-function nameValidation() {
-    if (!workerIdentity.value) {
-        workerIdentity.style.border = "5px solid";
-        workerIdentity.style.borderColor = "red";
-        showValidationMessage(workerIdentity, 'This field should not be empty.');
+function showValidationMessagePassword(passwordInput, message) {
+    let messageElement = passwordInput.nextElementSibling;
+    if (!messageElement || !messageElement.classList.contains('validation-message')) {
+        messageElement = document.createElement('span');
+        messageElement.classList.add('validation-message');
+        messageElement.style.color = 'red';
+        passwordInput.parentNode.insertBefore(messageElement, passwordInput.nextElementSibling);
+    }
+
+    messageElement.textContent = message;
+}
+
+function clearValidationMessage(workerIdentityInput, passwordInput) {
+    let workerIdentity = workerIdentityInput.nextElementSibling;
+    let password = passwordInput.nextElementSibling;
+    if (workerIdentity.classList.contains('validation-message')) {
+        workerIdentity.textContent = '';
+        workerIdentity.classList.remove('validation-message');
+    }
+    if (password.classList.contains('validation-message')) {
+        password.textContent = '';
+        password.classList.remove('validation-message');
     }
 }
 
+function validation() {
+
+    if (!workerIdentity.value) {
+        workerIdentity.style.border = "5px solid";
+        workerIdentity.style.borderColor = "red";
+        showValidationMessageWorkerIdentity(workerIdentity, 'You should write your identity.');
+    }
+    if (!password.value) {
+        password.style.border = "5px solid";
+        password.style.borderColor = "red";
+        showValidationMessagePassword(password, 'You should write your password.');
+    }
+    if (workerIdentity.value) {
+        workerIdentity.style.border = "";
+        clearValidationMessage(workerIdentity, password);
+    }
+    if (password.value) {
+        password.style.border = "";
+        clearValidationMessage(password);
+    }
+}
+
+
 submitButton.addEventListener('click', e => {
     e.preventDefault();
-    nameValidation();
+    validation();
 });
